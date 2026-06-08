8326ab134a56129894f88d5197ce9e6853fed548
ErikDalgard <erik.dalgard.6689@student.uu.se>
2026-06-08 13:00:14 +0200

    Added architecture for 3D and 2D models, as well as a training ipynb file.

---
+++ notebooks/models_2D.py
@@ -0,0 +1,101 @@
+# %%
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# %%
+model_1_input = (20, 20, 6)
+model_2_input = (30, 30, 10)
+model_3_input = (40, 40, 26)
+
+# %%
+#ARCHI-1
+#-------
+archi_1_2D = keras.Sequential([
+    #Input layer, input (20x20x6)
+    layers.Input(shape=model_1_input),
+
+    #BLOCK 1
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+    #1st Pooling, reducing 20x20 to 10x10
+    layers.MaxPooling2D(pool_size=2),
+
+    #BLOCK 2
+    layers.Conv2D(64, kernel_size=(1, 1), activation='relu', padding='same'),
+
+    #BLOCK 3
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+    #2nd Pooling, reducing 10x10 to 5x5
+    layers.MaxPooling2D(pool_size=2),
+
+    #BLOCK 4
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+
+    #Classification block
+    layers.Flatten(),
+    layers.Dense(150, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+# %%
+#ARCHI-2
+#-------
+
+archi_2_2D = keras.Sequential([
+    #Input layer, input (30x30x10)
+    layers.Input(shape=model_2_input),
+
+    #BLOCK 1
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+    #1st Pooling, reducing 30x30 to 15x15
+    layers.MaxPooling2D(pool_size=2), 
+
+    #BLOCK 2
+    layers.Conv2D(64, kernel_size=(2, 2), activation='relu', padding='same'),
+
+    #BLOCK 3
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+    #2nd Pooling, reducing 15x15 to 7x7
+    layers.MaxPooling2D(pool_size=2),
+
+    #BLOCK 4
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+
+    #Classification block
+    layers.Flatten(),
+    layers.Dense(250, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+# %%
+#ARCHI-3
+#-------
+
+archi_3_2D = keras.Sequential([
+    #Input layer, input (40x40x26)
+    layers.Input(shape=model_3_input),
+
+    #BLOCK 1
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+    #1st Pooling, reducing 40x40 to 20x20
+    layers.MaxPooling2D(pool_size=2), 
+
+    #BLOCK 2
+    layers.Conv2D(64, kernel_size=(2, 2), activation='relu', padding='same'),
+
+    #BLOCK 3
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+    #2nd Pooling, reducing 20x20 to 10x10
+    layers.MaxPooling2D(pool_size=2), 
+
+    #BLOCK 4
+    layers.Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'),
+
+    #Classification block
+    layers.Flatten(),
+    layers.Dense(250, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+
+
---
+++ notebooks/models_3D.py
@@ -0,0 +1,98 @@
+# %%
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# %%
+model_1_input = (20, 20, 6, 1)
+model_2_input = (30, 30, 10, 1)
+model_3_input = (40, 40, 26, 1)
+
+# %%
+#ARCHI-1
+#-------
+archi_1_3D = keras.Sequential([
+    #Input layer, input (20x20x6)
+    layers.Input(shape=model_1_input),
+
+    #BLOCK 1
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+    #1st Pooling, reducing 20x20x6 to 10x10x3
+    layers.MaxPooling3D(pool_size=2),
+
+    #BLOCK 2
+    layers.Conv3D(64, kernel_size=(1, 1, 1), activation='relu', padding='same'),
+
+    #BLOCK 3
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+    #2nd Pooling, reducing 10x10x3 to 5x5x1
+    layers.MaxPooling3D(pool_size=2),
+
+    #BLOCK 4
+    layers.Conv3D(64, kernel_size=(5, 5, 1), activation='relu', padding='same'),
+    
+    #Classification block
+    layers.GlobalAveragePooling3D(),
+    layers.Dense(150, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+# %%
+#ARCHI-2
+#-------
+
+archi_2_3D = keras.Sequential([
+    #Input layer, input (30x30x10)
+    layers.Input(shape=model_2_input),
+
+    #BLOCK 1
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+    #1st Pooling, reducing 30x30x10 to 15x15x5
+    layers.MaxPooling3D(pool_size=2), 
+
+    #BLOCK 2
+    layers.Conv3D(64, kernel_size=(2, 2, 1), activation='relu', padding='same'),
+
+    #BLOCK 3
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+    #2nd Pooling, reducing 15x15x5 to 7x7x2
+    layers.MaxPooling3D(pool_size=2),
+
+    #BLOCK 4
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+
+    #Classification block
+    layers.GlobalAveragePooling3D(),
+    layers.Dense(250, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+# %%
+#ARCHI-3
+#-------
+
+archi_3_3D = keras.Sequential([
+    #Input layer, input (40x40x26)
+    layers.Input(shape=model_3_input),
+
+    #BLOCK 1
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+    #1st Pooling, reducing 40x40x26 to 20x20x13
+    layers.MaxPooling3D(pool_size=2), 
+
+    #BLOCK 2
+    layers.Conv3D(64, kernel_size=(2, 2, 2), activation='relu', padding='same'),
+
+    #BLOCK 3
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+    #2nd Pooling, reducing 20x20x13 to 10x10x6
+    layers.MaxPooling3D(pool_size=2), 
+
+    #BLOCK 4
+    layers.Conv3D(64, kernel_size=(5, 5, 3), activation='relu', padding='same'),
+
+    #Classification block
+    layers.GlobalAveragePooling3D(),
+    layers.Dense(250, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
